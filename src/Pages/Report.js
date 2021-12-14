c8c11ce120bd205a48c31dd1f9f3b34f3813995e
codingSlate <ananta2021k@gmail.com>
2021-12-14 20:50:41 +0530

    1 file modified

--- src/Pages/Report.js
+++ src/Pages/Report.js
@@ -4,7 +4,8 @@ const Report = () => {
   const data = weatherData(scale);
   return (
     <section>
-      <h1>Your weather report in</h1>
+      <h1>Your weather report in {
+        scale.charAt(0).toUpperCase() + scale.slice(1)}</h1>
     </section>
   );
 };
