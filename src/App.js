4aeb11c560408569b0a1d733d40ee4f2fa9596df
codingSlate <ananta2021k@gmail.com>
2021-12-14 19:33:05 +0530

    footer Link added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React  from 'react';
+import React from 'react';
 import { BrowserRouter as MyRouter, Route } from 'react-router-dom';
-
 import Home from './Pages/Home';
 import About from './Pages/About';
+import Footer from './Pages/Footer';
 import './style.css';
 
 const App = () => {
   return (
     <MyRouter>
       <div>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
         <main>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
         </main>
+        <Footer />
       </div>
     </MyRouter>
   );
