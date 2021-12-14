8e2126dc824e441193772dc3ecd1ccf104561a1f
codingSlate <ananta2021k@gmail.com>
2021-12-14 22:18:34 +0530

    Router parameter created

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
-const Home = () =>{
-    return(
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Home = () => {
+  return (
     <section>
       <h1>Welcome to Weather Report </h1>
+      <nav aria-label="main">
+        <p>View Weather in </p>
+        <ul>
+          <li>
+            <Link to="/reports/kelvin">Kelvin</Link>
+          </li>
+          <li>
+            <Link to="/reports/celcious">Celcious</Link>
+          </li>
+          <li>
+            <Link to="/reports/fahrenheit">Fahrenheit</Link>
+          </li>
+        </ul>
+      </nav>
     </section>
-    )
-}
-export default Home
+  );
+};
+export default Home;
--- src/Pages/Report.js
+++ src/Pages/Report.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { weatherData } from './weatherData';
-const Report = ({ scale }) => {
+const Report = ({ match }) => {
+  const { scale } = match.params;
   const data = weatherData(scale);
   return (
     <section>
@@ -8,9 +9,9 @@ const Report = ({ scale }) => {
         Your weather report in {scale.charAt(0).toUpperCase() + scale.slice(1)}
       </h1>
       {data.map((datapoint) => (
-        <Fragment >
+        <Fragment>
           <div key={datapoint.state} className="row">
-            <div >{datapoint.state} </div>
+            <div>{datapoint.state} </div>
             <span>{datapoint.temp.toString()}</span>
           </div>
         </Fragment>
