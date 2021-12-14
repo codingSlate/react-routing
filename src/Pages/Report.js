9aca06da9e939275e27357d0216fe9750fe33581
codingSlate <ananta2021k@gmail.com>
2021-12-14 21:25:44 +0530

    3 files modified

--- src/Pages/Report.js
+++ src/Pages/Report.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { weatherData } from './weatherData';
-const Report = () => {
+const Report = ({ scale }) => {
   const data = weatherData(scale);
   return (
     <section>
-      <h1>Your weather report in {
-        scale.charAt(0).toUpperCase() + scale.slice(1)}</h1>
+      <h1>
+        Your weather report in {scale.charAt(0).toUpperCase() + scale.slice(1)}
+      </h1>
+      <dl>
+        {data.map((datapoint) => (
+          <Fragment key={datapoint.id}>
+            <dt>{datapoint.state} </dt>
+            <dd>{datapoint.temp.toString()}</dd>
+          </Fragment>
+        ))}
+      </dl>
     </section>
   );
 };
