d30cfd8490004cd9381b28ca0282629f16b9ee02
codingSlate <ananta2021k@gmail.com>
2021-12-14 22:31:42 +0530

    section Heading component template done

--- src/Pages/Report.js
+++ src/Pages/Report.js
@@ -5,12 +5,11 @@ import Section from './Section';
 const Report = ({ match }) => {
   const { scale } = match.params;
   const data = weatherData(scale);
+  const headingText = `Your weather report in ${
+    scale.charAt(0).toUpperCase() + scale.slice(1)
+  }`;
   return (
-    <Section
-      headingText={`Your weather report in ${
-        scale.charAt(0).toUpperCase() + scale.slice(1)
-      }`}
-    >
+    <Section headingText={headingText}>
       {data.map((datapoint) => (
         <Fragment>
           <div key={datapoint.state} className="row">
