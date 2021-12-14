d81014464cdc9cd4b0d34c80ff97802cef8fe549
codingSlate <ananta2021k@gmail.com>
2021-12-14 19:24:48 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React  from 'react';
+import { BrowserRouter as MyRouter, Route } from 'react-router-dom';
+
 import Home from './Pages/Home';
 import About from './Pages/About';
-import { BrowserRouter as MyRouter, Route } from 'react-router-dom';
 import './style.css';
 
-const App = () => (
-  <MyRouter>
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <main>
-        <Home />
-        <About />
-      </main>
-    </div>
-  </MyRouter>
-);
+const App = () => {
+  return (
+    <MyRouter>
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <main>
+        </main>
+      </div>
+    </MyRouter>
+  );
+};
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Home = () =>{
     return(
     <section>
-      <h1>Welcome to Weather report </h1>
+      <h1>Welcome to Weather Report </h1>
     </section>
     )
 }
