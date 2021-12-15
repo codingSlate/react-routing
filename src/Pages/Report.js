9928b6ead62eba48b5910e4eac1078d5b5b1a152
codingSlate <ananta2021k@gmail.com>
2021-12-15 13:46:41 +0530

    routing completed

--- src/Pages/Report.js
+++ src/Pages/Report.js
@@ -4,6 +4,7 @@ import Section from './Section';
 
 const Report = ({ match }) => {
   const { scale } = match.params;
+  
   const data = weatherData(scale);
   const headingText = `Your weather report in ${
     scale.charAt(0).toUpperCase() + scale.slice(1)
