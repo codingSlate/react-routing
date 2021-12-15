8ad6f97f0ea64710e9f25b0a54a8bd38ea5c93c7
codingSlate <ananta2021k@gmail.com>
2021-12-15 14:12:38 +0530

    keyboard and screen reader using tabIndex -1 and useEffect and useRef

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # react-ekungg
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ekungg)
+
+
+## useRef
+- contains instances of mounted DOM elements
+- when the JSX is rendered the headingRef contain will be populated with the actual DOM element
+- then we can use imperative code on that element like JavaScript 
+
+
+
+## set focus
+- Anchor, button, input etc.
+- -1 or 0
+- -1 the browser allow JS to set focus on the element but the user not able to tab the element
--- src/Pages/Section.js
+++ src/Pages/Section.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Helmet from 'react-helmet';
-
 
 const Section = ({ children, headingText, headingLevel = 1 }) => {
   const H = `h${headingLevel}`;
+
+  const headingRef = useRef(null);
+
+  useEffect(() => {
+    headingRef.current.focus()
+    console.log(headingRef.current);
+  }, []);
+
   return (
     <section>
       <Helmet>
         <title>{`${headingText} | Ultimate Weather App`}</title>
       </Helmet>
-      <H>{headingText}</H>
+      {/* -1 or 0 */}
+      <H ref={headingRef} tabIndex="-1">
+        {headingText}
+      </H>
       {children}
     </section>
   );
